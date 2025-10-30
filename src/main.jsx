23218ebcb71d5b3ebb00259fca306eea650295b2
AyaanK077 <ayaanrk04@gmail.com>
2025-10-30 17:17:26 -0500

    Added profile page with back button and UI updates; integrated profile link into GamesList

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css"; 
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App.jsx";
-import PDP from "./PDP.jsx";
-import "./index.css";
+import GamesList from "./components/pages/GamesList.jsx";
+import GameDetail from "./components/pages/GameDetail.jsx";
+import Profile from "./components/pages/Profile.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/product/:id" element={<PDP />} />
+        <Route path="/" element={<GamesList />} />
+        <Route path="/product/:id" element={<GameDetail />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
