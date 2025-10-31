cee6742e55d38fa66721746cf34552643b9c81f1
RyanCogley <wowiec@me.com>
2025-10-31 11:56:00 -0500

    Update CartButton.jsx

--- src/components/shared/CartButton.jsx
+++ src/components/shared/CartButton.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useCart } from "../context/CartContext.jsx";
+import { useCart } from "../../context/CartContext.jsx";
 
 export default function CartButton() {
   const { cart, total } = useCart();
