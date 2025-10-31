cefa4e925e8311f2a5d51d23e8f6120111a8379a
RyanCogley <wowiec@me.com>
2025-10-31 11:27:04 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import GameDetail from "./components/pages/GameDetail";
+import Profile from "./components/pages/Profile";
+import Wishlist from "./components/pages/Wishlist";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import Buy from "./components/pages/Buy";
+import NotFound from "./components/pages/NotFound";
+import CartButton from "./components/shared/CartButton";
+import UserButton from "./components/shared/UserButton";
+
+const LOCAL_GAMES = [/* your LOCAL_GAMES data here */];
+
 export default function App() {
   const [games, setGames] = useState(LOCAL_GAMES);
   const [cart, setCart] = useState([]);
@@ -40,3 +55,4 @@ export default function App() {
     </div>
   );
 }
+
