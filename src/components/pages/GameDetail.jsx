1908ca0ef588192137e88165072304937bc97f85
RyanCogley <wowiec@me.com>
2025-10-31 12:32:03 -0500

    forgot two ups lol

--- src/components/pages/GameDetail.jsx
+++ src/components/pages/GameDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { useCart } from "../context/CartContext.jsx";
+import { useCart } from "../../context/CartContext.jsx";
 
 export default function ProductPage() {
   const { id } = useParams();
