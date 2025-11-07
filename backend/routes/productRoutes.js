e5ebf8d74d3bb31837a3f63e3552e4ff38ed4fae
Alp Bayrak <142843377+AlpBayrak61@users.noreply.github.com>
2025-11-07 17:02:24 -0600

    Create productRoutes.js

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,15 @@
+// /routes/productRoutes.js
+const express = require('express');
+const ctrl = require('../controllers/productsController');
+
+const router = express.Router();
+
+// Create Read Update Delete (CRUD)
+//two reads, one for all products, one read by specifying product id
+router.get('/', ctrl.getAllProducts);
+router.get('/:id', ctrl.getProductById);
+router.post('/', ctrl.createProduct);
+router.patch('/:id', ctrl.updateProduct);
+router.delete('/:id', ctrl.deleteProduct);
+
+module.exports = router;
