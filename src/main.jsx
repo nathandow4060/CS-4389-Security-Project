3733cf0b87480881701790b3ee90b850b0dd1f38
AyaanK077 <ayaanrk04@gmail.com>
2025-11-13 17:40:42 -0600

    Resolve main.jsx merge conflict using main branch structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
-import "./index.css"; 
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import GamesList from "./components/pages/GamesList.jsx";
-import GameDetail from "./components/pages/GameDetail.jsx";
-import Profile from "./components/pages/Profile.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<GamesList />} />
-        <Route path="/product/:id" element={<GameDetail />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
-=======
 import App from "./App.jsx";
 import { CartProvider } from "./context/CartContext.jsx";
 import "./index.css";
@@ -27,6 +9,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <CartProvider>
       <App />
     </CartProvider>
->>>>>>> ba582f12a7958d9acb102e17983cc1d9ae9e7610
   </React.StrictMode>
 );
