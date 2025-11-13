c5932bcb80e7cd2621654a822e83f361ec789f7b
RyanCogley <wowiec@me.com>
2025-11-13 08:02:14 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
-import PDP from "./PDP.jsx";
-import Buy from "./components/pages/Buy.jsx";
 import { CartProvider } from "./context/CartContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CartProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<App />} />
-          <Route path="/products/:id" element={<PDP />} />
-          <Route path="/Buy" element={<Buy />} />
-        </Routes>
-      </BrowserRouter>
+      <App />
     </CartProvider>
   </React.StrictMode>
 );
